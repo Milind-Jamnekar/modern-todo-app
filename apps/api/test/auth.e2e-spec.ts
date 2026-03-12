323124bce33c5c2d96b98ab617f89bc153164317
Milind Jamnekar <milindjamnekar502@gmail.com>
2026-03-12 23:10:26 +0530

    fix: update import statement for supertest in auth.e2e-spec.ts chore: enable esModuleInterop and allowSyntheticDefaultImports in tsconfig

--- apps/api/test/auth.e2e-spec.ts
+++ apps/api/test/auth.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { INestApplication } from '@nestjs/common';
-import *  as request from 'supertest';
+import request from 'supertest';
 import { createTestApp } from './setup';
 
 describe('Auth (e2e)', () => {
